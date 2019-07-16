5544b992c1020a64059e11408a0761a92be9d166
Vishv Verma <vishvverma@gmail.com>
2019-07-16 17:35:35 -0400

    lab17 Secure Password

---
+++ passwd/firstpasswd.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3 # A simple Python program to demonstrate getpass.getpass() to read password import getpass
+import getpass
+def main():
+    p = getpass.getpass() 
+    print("Password entered:", p)
+
+main()
