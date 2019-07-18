b9a99cbb1e0d49d134a17ab0aac37e9c51f740bd
Vishv Verma <vishvverma@gmail.com>
2019-07-18 18:42:22 -0400

    lab40 Flask

---
+++ flaskapi/milkncookies.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+from flask import Flask, make_response, request, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+   return render_template("index.html")
+
+@app.route("/setcookie", methods = ["POST", "GET"])
+def setcookie():
+   if request.method == "POST":
+     user = request.form["nm"]
+
+   resp = make_response(render_template("readcookie.html"))
+   resp.set_cookie("userID", user)
+
+   return resp
+
+@app.route("/getcookie")
+def getcookie():
+   name = request.cookies.get("userID")
+   return '<h1>welcome '+name+'</h1>'
+
+
+if __name__ == "__main__":
+    app.run(port=5006)
